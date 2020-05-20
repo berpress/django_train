af1104416bb0af6ca0c493f0865e1b159527fcba
litovsky <berpress@gmail.com>
2020-05-20 22:45:19 +0300

    add Cities table

--- Makefile
+++ Makefile
@@ -4,3 +4,10 @@ start:
 env:
 	source ./env/bin/activate
 
+migrate:
+	@poetry run src/manage.py migrate
+
+make_migrate:
+	@poetry run src/manage.py makemigrations
+
+
--- src/cities/models.py
+++ src/cities/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=99, unique=True, verbose_name='City')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        pass
