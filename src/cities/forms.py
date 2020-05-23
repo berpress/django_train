37e752fc3e9b37e1ddb85dfae856c16e2ec7d4d4
berpress <berpress@gmail.com>
2020-05-23 14:45:24 +0300

    Add cities form

--- src/cities/forms.py
+++ src/cities/forms.py
@@ -1,5 +1,17 @@
 from django import forms
+from .models import City
 
 
-class HtmlForm(forms.Form):
-    name = forms.CharField(label='City')
+# class HtmlForm(forms.Form):
+#     name = forms.CharField(label='City')
+
+
+class CityForm(forms.ModelForm):
+    name = forms.CharField(label='City',
+                           widget=forms.
+                           TextInput(attrs={'class': 'form-control',
+                                            'placeholder': 'Enter city name'}))
+
+    class Meta(object):
+        model = City
+        fields = ('name', )
--- src/cities/views.py
+++ src/cities/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from .models import City
 from django.views.generic import DetailView
-from .forms import HtmlForm
+from django.views.generic.edit import CreateView
+from .forms import CityForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 
 def home(request):
     if request.method == 'POST':
-        form = HtmlForm(request.POST or None)
+        form = CityForm(request.POST or None)
         if form .is_valid():
             print(form.cleaned_data)
-    form = HtmlForm()
+    form = CityForm()
     cities = City.objects.all()
     return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
 
@@ -19,3 +21,10 @@ class CityDetailView(DetailView):
     queryset = City.objects.all()
     context_object_name = 'object'
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('city:home')
