06e0126bf7053f89b9dbb631afb1e40fbec1c8b7
litovsky <berpress@gmail.com>
2020-05-23 12:23:54 +0300

    Add submit form for cities

--- src/cities/views.py
+++ src/cities/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from .models import City
+from django.views.generic import DetailView
+from .forms import HtmlForm
 # Create your views here.
 
 
 def home(request):
+    if request.method == 'POST':
+        form = HtmlForm(request.POST or None)
+        if form .is_valid():
+            print(form.cleaned_data)
+    form = HtmlForm()
     cities = City.objects.all()
-    return render(request, 'cities/home.html', {'objects_list': cities})
+    return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    context_object_name = 'object'
+    template_name = 'cities/detail.html'
