37a89a57c35f4b7582d3691e1e13c4821a7b6db8
berpress <berpress@gmail.com>
2020-06-09 17:47:30 +0300

    Add routes

--- routes/models.py
+++ routes/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from trains.models import Train
 
 
 class Route(models.Model):
     name = models.CharField(max_length=99, unique=True, verbose_name="Name route")
     from_city = models.CharField(max_length=99, verbose_name="From")
     to_city = models.CharField(max_length=99, verbose_name="From")
+    across_cities = models.ManyToManyField(
+        Train, blank=True, verbose_name="Across cities"
+    )
+    travel_times = models.IntegerField(verbose_name="Travels times")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Route"
+        verbose_name_plural = "Routers"
+        ordering = ["name"]
