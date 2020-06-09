4b22e5dd31173d091fa80d84094ab1a5eaddcee1
berpress <berpress@gmail.com>
2020-06-09 10:17:36 +0300

    Fix

--- trains/models.py
+++ trains/models.py
@@ -5,30 +5,29 @@ from cities.models import City
 
 
 class Train(models.Model):
-    name = models.CharField(max_length=99, unique=True,
-                            verbose_name='Number train')
-    from_city = models.ForeignKey(City, on_delete=models.CASCADE,
-                                  verbose_name='From',
-                                  related_name='from_city')
-    to_city = models.ForeignKey(City, on_delete=models.CASCADE,
-                                verbose_name='To', related_name='to_city')
-    travel_time = models.IntegerField(verbose_name='hours')
+    name = models.CharField(max_length=99, unique=True, verbose_name="Number train")
+    from_city = models.ForeignKey(
+        City, on_delete=models.CASCADE, verbose_name="From", related_name="from_city"
+    )
+    to_city = models.ForeignKey(
+        City, on_delete=models.CASCADE, verbose_name="To", related_name="to_city"
+    )
+    travel_time = models.IntegerField(verbose_name="hours")
 
     def __str__(self):
-        return f'Train {self.name} from {self.from_city} to {self.to_city}'
+        return f"Train {self.name} from {self.from_city} to {self.to_city}"
 
     class Meta:
-        verbose_name = 'Train'
-        verbose_name_plural = 'Trains'
-        ordering = ['name']
+        verbose_name = "Train"
+        verbose_name_plural = "Trains"
+        ordering = ["name"]
 
     def clean(self, *args, **kwargs):
         if self.to_city == self.from_city:
-            raise ValidationError('Check city')
-        qs = Train.objects.filter(from_city = self.from_city,
-                                  to_city=self.to_city,
-                                  travel_time=self.travel_time).\
-            exclude(pk=self.pk)
+            raise ValidationError("Check city")
+        qs = Train.objects.filter(
+            from_city=self.from_city, to_city=self.to_city, travel_time=self.travel_time
+        ).exclude(pk=self.pk)
         if qs.exists():
-            raise ValidationError('Change time')
+            raise ValidationError("Change time")
         return super(Train, self).clean(*args, **kwargs)
