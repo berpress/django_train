a841acea646aaa8eb6a6c3317a581e309d671a77
Litovsky <berpress@gmail.com>
2020-06-10 22:36:24 +0300

    Add multi choose in routes

--- routes/forms.py
+++ routes/forms.py
@@ -7,19 +7,31 @@ class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(
         label="From",
         queryset=City.objects.all(),
-        widget=forms.Select(attrs={"class": "form-control js-example-basic-single"}),
+        widget=forms.Select(
+            attrs={
+                "class": "form-control js-example-basic-single "
+                "js-example-basic-single"
+            }
+        ),
     )
     to_city = forms.ModelChoiceField(
         label="To",
         queryset=City.objects.all(),
-        widget=forms.Select(attrs={"class": "form-control js-example-basic-single"}),
+        widget=forms.Select(
+            attrs={
+                "class": "form-control js-example-basic-single js-example-basic-single"
+            }
+        ),
     )
     across_cities = forms.ModelMultipleChoiceField(
         label="Across cities",
         queryset=City.objects.all(),
         required=False,
         widget=forms.SelectMultiple(
-            attrs={"class": "form-control js-example-basic-multiple"}
+            attrs={
+                "class": "form-control js-example-basic-multiple "
+                "js-example-basic-multiple"
+            }
         ),
     )
     travelling_time = forms.IntegerField(
